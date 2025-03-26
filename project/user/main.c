79356f84171aa0c5f149d313620c82c111df613d
TaoMing <213223263@seu.edu.cn>
2025-03-26 12:29:07 +0800

    添加了GPS功能

--- project/user/main.c
+++ project/user/main.c
@@ -48,6 +48,9 @@ void main()
     unsigned char index=0;  //循环变量
     clock_init(SYSTEM_CLOCK_40M); // 务必保留
 		debug_init(); // 务必保留
+  
+    //GPS初始化
+    gps_init();
 
     //屏幕初始化
 		ips114_init();
@@ -68,26 +71,66 @@ void main()
     ips114_show_char(220,16*1,'g');
   
     //角速度
+    ips114_set_color(RGB565_PURPLE,RGB565_WHITE);
     ips114_show_string(0,16*2,"Gyro: x,y,z");
     ips114_show_char(60,16*3,'d');
     ips114_show_char(140,16*3,'d');
     ips114_show_char(220,16*3,'d');
-    ips114_set_color(RGB565_RED,RGB565_WHITE);
+
+    
+    //GPS
+    ips114_set_color(RGB565_YELLOW,RGB565_WHITE);
+    ips114_show_string(0, 16*5, "S: ");//定位状态
+    ips114_show_string(40, 16*5,"N: ");//卫星数目
+    ips114_show_string(100, 16*5,"H: ");//GPS天线高度
+
+    ips114_show_string(0, 16*6, "Lat");//纬度
+    ips114_show_string(120, 16*6, "Lon");//经度
+    ips114_show_string(0, 16*7, "v");//速度
+    ips114_show_string(120, 16*7, "d");//方向
+
     
     //设置中断回调函数 5ms采样一次IMU数据
     tim0_irq_handler = pit_hanlder_imu;	
     pit_ms_init(PIT_IMU, 5);
 	
-		
+		ips114_set_color(RGB565_RED,RGB565_WHITE);
     while(1)
 		{
+      //IMU
       for(index=0;index<3;index++)
       {
         ips114_show_float(80*index, 16*1 ,imu_acc[index],2,3);//加速度信息 字符8x16
         ips114_show_float(80*index, 16*3 ,imu_gyro[index],2,3);//角速度信息
       }
+      //GPS
+      if(gps_tau1201_flag)
+      {
+        gps_tau1201_flag = 0;
+        if(!gps_data_parse())          //开始解析数据
+        {
+				//显示GPS位置信息
+        ips114_show_uint8(20, 16*5, gps_tau1201.state);
+        ips114_show_uint8(60, 16*5, gps_tau1201.satellite_used);
+        ips114_show_float(120, 16*5, gps_tau1201.height, 4, 6);
+          
+        ips114_show_float(20, 16*6, gps_tau1201.latitude, 4, 6);
+        ips114_show_float(140, 16*6, gps_tau1201.longitude, 4, 6);
+        ips114_show_float(20, 16*7, gps_tau1201.speed, 4, 6);
+        ips114_show_float(140, 16*7, gps_tau1201.direction, 4, 6);
+
+				
+//				printf("gps_state       = %d\r\n" , gps_tau1201.state);         //输出当前定位有效模式 1：定位有效  0：定位无效
+//				printf("latitude        = %lf\r\n", gps_tau1201.latitude);      //输出纬度信息
+//				printf("longitude       = %lf\r\n", gps_tau1201.longitude);     //输出经度信息
+//				printf("speed           = %lf\r\n", gps_tau1201.speed);         //输出速度信息
+//				printf("direction       = %lf\r\n", gps_tau1201.direction);     //输出方向信息
+//				printf("satellite_used  = %d\r\n" , gps_tau1201.satellite_used);//输出当前用于定位的卫星数量
+//				printf("height          = %lf\r\n", gps_tau1201.height);        //输出当前GPS天线所处高度
+        }
+      }
       system_delay_ms(1000);
-    }
+     }
 }
 /**
   * @brief TIM0中断处理函数，提取imu数据
